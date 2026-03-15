b54972e76dc1f1f4f7d64027b3126699383cf2b4
Adesh Kumar <89107712+adeshkumar1@users.noreply.github.com>
2026-03-15 01:37:29 -0400

    leetcode nvim

--- nvim/.config/nvim/lua/Adesh/core/options.lua
+++ nvim/.config/nvim/lua/Adesh/core/options.lua
@@ -2,7 +2,8 @@
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
 
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
+vim.opt.guicursor = ""
+
 vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#ffffff" })
 vim.api.nvim_set_hl(0, "iCursor", { fg = "#000000", bg = "#ffffff" })
 vim.opt.nu = true
@@ -44,23 +45,23 @@ vim.opt.mouse = "a"
 vim.g.editorconfig = true
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = {
-        "javascript",
-        "typescript",
-        "javascriptreact",
-        "typescriptreact",
-        "json",
-        "html",
-        "css",
-        "yaml",
-        "markdown",
-    },
-    callback = function()
-        vim.opt_local.expandtab = true
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-        vim.opt_local.shiftwidth = 2
-    end,
+	pattern = {
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"json",
+		"html",
+		"css",
+		"yaml",
+		"markdown",
+	},
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
 })
 
 vim.g.tmux_navigator_no_wrap = 1
--- nvim/.config/nvim/lua/Adesh/plugins/leetcode.lua
+++ nvim/.config/nvim/lua/Adesh/plugins/leetcode.lua
@@ -1,11 +1,11 @@
 return {
 	{
 		"3rd/image.nvim",
-		lazy = true,
+		lazy = false,
 		opts = {
 			backend = "kitty",
 			integrations = {
-				markdown = { enabled = false },
+				markdown = { enabled = true },
 				neorg = { enabled = false },
 			},
 			tmux_show_only_in_active_window = true,
@@ -20,8 +20,33 @@ return {
 			"3rd/image.nvim",
 		},
 		opts = {
-			lang = "python3",
+			lang = "cpp",
 			image_support = true,
+			injector = {
+				["cpp"] = {
+					before = {
+						"#include <iostream>",
+						"#include <vector>",
+						"#include <string>",
+						"#include <algorithm>",
+						"#include <unordered_map>",
+						"#include <map>",
+						"#include <set>",
+						"#include <queue>",
+						"#include <deque>",
+						"#include <stack>",
+						"#include <numeric>",
+						"using namespace std;",
+					},
+				},
+			},
+			keys = {
+				{ "q", "<cmd>Leet exit<cr>", mode = { "n" }, desc = "Exit LeetCode" },
+				{ "<leader>lt", "<cmd>Leet test<cr>", mode = { "n" }, desc = "LeetCode Test" },
+				{ "<leader>ls", "<cmd>Leet submit<cr>", mode = { "n" }, desc = "LeetCode Submit" },
+				{ "<leader>li", "<cmd>Leet info<cr>", mode = { "n" }, desc = "LeetCode Info" },
+				{ "<leader>lr", "<cmd>Leet run<cr>", mode = { "n" }, desc = "LeetCode Run" },
+			},
 		},
 	},
 }
