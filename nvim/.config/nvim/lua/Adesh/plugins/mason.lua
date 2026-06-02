167931ff78b911e351c9e0e7aee95d3b04558ff4
Adesh Kumar <adeshkumar@stripe.com>
2026-06-01 17:39:04 -0700

    Fix treesitter and add mason-lspconfig for auto-installing LSP servers
    Committed-By-Agent: claude

--- nvim/.config/nvim/lua/Adesh/plugins/mason.lua
+++ nvim/.config/nvim/lua/Adesh/plugins/mason.lua
@@ -1,14 +1,35 @@
 return {
-	"williamboman/mason.nvim",
-	config = function()
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
 				},
-			},
-		})
-	end,
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"gopls",
+					"jdtls",
+					"ruby_lsp",
+					"rust_analyzer",
+					"lua_ls",
+					"ts_ls",
+					"kotlin_language_server",
+					"clangd",
+				},
+				automatic_installation = true,
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/Adesh/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/Adesh/plugins/treesitter.lua
@@ -2,9 +2,18 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
-		build = ":TSUpdate bash html javascript json lua python tsx typescript vim yaml cpp make cmake rust c css kotlin go java ruby",
+		build = ":TSUpdate",
 		config = function()
-			require("nvim-treesitter").setup()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash", "html", "javascript", "json", "lua", "python",
+					"tsx", "typescript", "vim", "yaml", "cpp", "make", "cmake",
+					"rust", "c", "css", "kotlin", "go", "java", "ruby",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
 		end,
 	},
 }
